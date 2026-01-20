f895ee7f0d889ea58828e40df418b726851612b3
namakemono248 <namakemono248@gmail.com>
2026-01-20 22:05:19 +0900

    Create 魔法文法初期.cs

---
+++ 魔法文法初期.cs
@@ -0,0 +1,11 @@
+[System.Serializable]
+public class MagicData
+{
+    // 基本ステータス
+    public float damage = 0f;
+    public float speed = 0f;
+    public float duration = 0f;
+
+    // フラグ系
+    public bool explosive;
+}
