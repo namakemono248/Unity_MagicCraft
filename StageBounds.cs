d3d002ab4bc5b75f3324921880b88a036e35490f
namakemono248 <namakemono248@gmail.com>
2026-01-20 22:24:17 +0900

    ステージのボーダーを作る

---
+++ StageBounds.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class StageBounds : MonoBehaviour
+{
+    public float width = 50f;
+    public float height = 50f;
+
+    public float MinX => transform.position.x - width  * 0.5f;
+    public float MaxX => transform.position.x + width  * 0.5f;
+    public float MinY => transform.position.y - height * 0.5f;
+    public float MaxY => transform.position.y + height * 0.5f;
+
+    public bool Contains(Vector2 pos)
+    {
+        return pos.x >= MinX && pos.x <= MaxX
+            && pos.y >= MinY && pos.y <= MaxY;
+    }
+
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0));
+    }
+#endif
+}
