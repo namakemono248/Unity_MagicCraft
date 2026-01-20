898df8c4301bca3f2fad0afebd64bbd56bc68c65
namakemono248 <namakemono248@gmail.com>
2026-01-20 22:04:09 +0900

    Create プレイヤー移動.cs

---
+++ プレイヤー移動.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class MagicCaster : MonoBehaviour
+{
+    private PlayerMove playerMove;
+
+    void Awake()
+    {
+        playerMove = GetComponent<PlayerMove>();
+    }
+
+    public void Cast(RuneGrammar grammar)
+    {
+        if (GameStateTest.isCrafting) return;
+        if (grammar == null) return;
+
+        if (grammar.projectilePrefab == null)
+        {
+            Debug.LogWarning("RuneGrammar に projectilePrefab が設定されていません！");
+            return;
+        }
+
+        MagicData data = new MagicData();
+        foreach (var rune in grammar.runes)
+            rune.Apply(data);
+
+        GameObject proj = Instantiate(
+            grammar.projectilePrefab,
+            transform.position,
+            Quaternion.identity
+        );
+
+        Vector2 dir = playerMove.lastDirection;
+        if (dir != Vector2.zero)
+            proj.transform.right = dir;
+
+        var mp = proj.GetComponent<MagicProjectile>();
+        if (mp != null)
+            mp.SetData(data);
+    }
+}
