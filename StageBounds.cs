521b379768896b69ae34c7af6f513734d49032ba
namakemono248 <namakemono248@gmail.com>
2026-01-23 16:24:55 +0900

    ステージごとのボーダーの設定
    Removed redundant MinX and MaxX property definitions.

--- StageBounds.cs
+++ StageBounds.cs
@@ -5,16 +5,10 @@ public class StageBounds : MonoBehaviour
     public float width = 50f;
     public float height = 50f;
 
-    public float MinX => transform.position.x - width  * 0.5f;
-    public float MaxX => transform.position.x + width  * 0.5f;
+    public float MinX => transform.position.x - width * 0.5f;
+    public float MaxX => transform.position.x + width * 0.5f;
     public float MinY => transform.position.y - height * 0.5f;
     public float MaxY => transform.position.y + height * 0.5f;
-
-    public bool Contains(Vector2 pos)
-    {
-        return pos.x >= MinX && pos.x <= MaxX
-            && pos.y >= MinY && pos.y <= MaxY;
-    }
 
 #if UNITY_EDITOR
     void OnDrawGizmos()
